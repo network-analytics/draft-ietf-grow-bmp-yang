d07f4dd3aaf12c80a1f47181a9c470632b0220a8
jccardonar <jccardona82@gmail.com>
2025-06-12 15:25:35 -0500

    Version 05 (#5)
    * cleaning repo
    
    * cleaning-repo
    
    * version 0.1 of this version
    
    * version 5, adding new autheros
    
    * formating of xml, fixing some tags, adn do some spell checking
    
    * fixing text
    
    * before another pass
    
    * Last commit before merging
    
    * fixing model and others

--- validate-all.sh
+++ validate-all.sh
@@ -1,3 +1,6 @@
 echo "Validating yang modes"
 
-pyang --ietf --max-line-length 69 ietf-bmp.yang
+pyang --ietf --max-line-length 69 ietf-bmp.yang ietf-bmp-bgp-dependencies.yang ietf-bmp-tcp-dependencies.yang
+
+yanglint -p . -p /Users/camilo/Documents/Projects/drafts/bmp_yang_model/other_examples/yang/experimental  ietf-bmp.yang ietf-bmp-bgp-dependencies.yang ietf-bmp-tcp-dependencies.yang -f yang > /dev/null
+
