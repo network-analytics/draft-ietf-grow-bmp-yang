41fddf4394094b6d897eb4e38542d50998651903
jccardonar <jccardona82@gmail.com>
2025-09-26 15:52:22 -0500

    before running canonical

--- validate-all.sh
+++ validate-all.sh
@@ -1,6 +1,17 @@
 echo "Validating yang modes"
 
-pyang --ietf --max-line-length 69 ietf-bmp.yang ietf-bmp-bgp-dependencies.yang ietf-bmp-tcp-dependencies.yang
+PYANG_INPUT='ietf-bmp.yang ietf-bmp-bgp-dependencies.yang ietf-bmp-tcp-dependencies.yang'
+
+pyang --ietf --max-line-length 69 $PYANG_INPUT
+
+# now make sure the format works	
+pyang -f yang --yang-canonical "$PYANG_INPUT" > /tmp/canonical.yang
+
+# Compare
+if ! diff -u "$orig" /tmp/canonical.yang; then
+  echo "ERROR: File is not canonical, run it with pyang -f yang --yang-canonical before"
+  exit 1
+fi
 
 yanglint -p . -p /Users/camilo/Documents/Projects/drafts/bmp_yang_model/other_examples/yang/experimental  ietf-bmp.yang ietf-bmp-bgp-dependencies.yang ietf-bmp-tcp-dependencies.yang -f yang > /dev/null
 
